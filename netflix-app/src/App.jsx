026359bf340cffa59f5409e58d44fcb5c45a07ef
Abed al salam Ashi <ashie@Abeds-MacBook-Air.local>
2022-12-29 03:59:20 +0200

    list the components

--- netflix-app/src/App.jsx
+++ netflix-app/src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import Header from "./components/Header";
+import Original from "./components/Original";
+import RandomMovie from "./components/RandomMovie";
+import TopRated from "./components/TopRated";
 
 const App = () => {
-  return;
+  return (
+    <>
+      <Header />
+      <RandomMovie />
+      <Original />
+      <TopRated />
+    </>
+  );
 };
 
 export default App;
