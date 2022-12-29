ee1a975f681f0c949d3de38df4c9e16b072a4e24
Abed al salam Ashi <ashie@Abeds-MacBook-Air.local>
2022-12-30 01:15:15 +0200

    routes

--- netflix-app/src/App.jsx
+++ netflix-app/src/App.jsx
@@ -1,16 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+
 import Header from "./components/Header";
-import Original from "./components/Original";
-import RandomMovie from "./components/RandomMovie";
-import TopRated from "./components/TopRated";
+import View from "./components/View";
+import MoviesList from "./components/MoviesList";
+import NotFound from "./components/NotFound";
 
 const App = () => {
   return (
     <>
       <Header />
-      <RandomMovie />
-      <Original />
-      <TopRated />
+      <Routes>
+        <Route path="/" element={<MoviesList />} />
+        <Route path="/Original/:id" element={<View />} />
+        <Route path="*" element={<NotFound />} />
+        {/* <Route path="/Top-Rated/:id" element={<View />} />
+        <Route path="/Trending/:id" /> */}
+      </Routes>
     </>
   );
 };
